ae89d2dc60758fdfffbc88b60c7842eb926b21af
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:06 -0500

    xml commenting

--- src/Soenneker.Blazor.Utils.LocalStorage/LocalStorageInterop.cs
+++ src/Soenneker.Blazor.Utils.LocalStorage/LocalStorageInterop.cs
@@ -123,6 +123,10 @@ public sealed class LocalStorageInterop : ILocalStorageInterop
         }
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async ValueTask DisposeAsync()
     {
         await _moduleImportUtil.DisposeContentModule(_modulePath);
