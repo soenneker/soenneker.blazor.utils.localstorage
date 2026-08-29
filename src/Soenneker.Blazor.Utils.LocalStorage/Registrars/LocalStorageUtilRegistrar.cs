eb27c6a36725b2e27d46be4a752e5ce9f90db1be
soenneker <jake@soenneker.com>
2026-08-29 14:16:53 -0500

    Improve public API XML documentation

--- src/Soenneker.Blazor.Utils.LocalStorage/Registrars/LocalStorageUtilRegistrar.cs
+++ src/Soenneker.Blazor.Utils.LocalStorage/Registrars/LocalStorageUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class LocalStorageUtilRegistrar
     /// <summary>
     /// Adds <see cref="ILocalStorageInterop"/> and <see cref="ILocalStorageUtil"/> as scoped services.
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddLocalStorageUtilAsScoped(this IServiceCollection services)
     {
         services.AddModuleImportUtilAsScoped()
